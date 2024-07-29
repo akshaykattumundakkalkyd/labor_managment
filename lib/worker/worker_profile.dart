afd71e2bdfae2d3fa53ce05fd0fec18c34cff965
dilshankunnummal <dilshankunnummal@gmail.com>
2024-07-29 15:07:08 +0530

    h

--- lib/worker/worker_profile.dart
+++ lib/worker/worker_profile.dart
@@ -166,6 +166,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:labor_managment/constants/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WorkerProfile extends StatefulWidget {
   const WorkerProfile({Key? key}) : super(key: key);
@@ -215,6 +216,50 @@ class _WorkerProfileState extends State<WorkerProfile> {
     fetchWorkerDetails();
   }
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void _showLogoutDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Logout'),
+          content: const Text('Are you sure you want to logout?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Cancel',
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Logout'),
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/roleSelection', (route) => false);
+                logout();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> logout() async {
+    _auth.signOut();
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('isAuthWorker');
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchUserDetails();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -224,6 +269,15 @@ class _WorkerProfileState extends State<WorkerProfile> {
           style: TextStyle(
               fontWeight: FontWeight.bold, color: secondaryColor, fontSize: 25),
         ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _showLogoutDialog();
+              },
+              icon: Icon(
+                Icons.logout_rounded,
+              ))
+        ],
         centerTitle: true,
         backgroundColor: primaryColor,
       ),
@@ -306,7 +360,7 @@ class _WorkerProfileState extends State<WorkerProfile> {
             ElevatedButton(
               onPressed: () async {
                 var updatedData =
-                    await Navigator.pushNamed(context, '/editWorker');
+                await Navigator.pushNamed(context, '/editWorker');
                 if (updatedData != null && updatedData is Map) {
                   setState(() {
                     username = updatedData['userName'] as String;
@@ -321,7 +375,7 @@ class _WorkerProfileState extends State<WorkerProfile> {
                 foregroundColor: primaryColor,
                 backgroundColor: secondaryColor,
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
